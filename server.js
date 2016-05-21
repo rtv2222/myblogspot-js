28ef4ecffb970633fb7ed91a05c1870378a64b55
Rajesh TV <rtv@cisco.com>
2016-05-21 13:38:50 +0530

    Initial commit of JS verticle

---
+++ server.js
@@ -0,0 +1,18 @@
+/**
+ * Created by rtv on 5/21/16.
+ */
+var Router = require("vertx-web-js/router");
+var server = vertx.createHttpServer();
+
+var router = Router.router(vertx);
+
+router.get("/").handler(function (ctx) {
+    // This handler will be called for "/" requests
+    var response = ctx.response();
+    response.putHeader("content-type", "text/plain");
+
+    // Write to the response and end it
+    response.end("Hello World!");
+});
+
+server.requestHandler(router.accept).listen(8080);
