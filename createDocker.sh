3df69d847a3ee8a0c38bf6125d31c8be46f58ac5
rtv2222 <rtv2222@gmail.com>
2016-05-30 22:18:31 +0530

    Update createDocker.sh

--- createDocker.sh
+++ createDocker.sh
@@ -2,7 +2,7 @@
 docker build -t rtv2222/user-service-js .
 if [ $? = 0 ]
 then
-   docker run -t -i -p 8888:8888 rtv2222/user-service-js &  
+   docker run -i -p 8888:8888 rtv2222/user-service-js &  
 else
    echo "Failed to build and deploy the docker container"
    exit 1
