ac6408a9e7bd760f0dda3b1b94d772236909e27c
rtv2222 <rtv2222@gmail.com>
2016-06-23 21:19:36 +0530

    Update createDocker.sh

--- createDocker.sh
+++ createDocker.sh
@@ -7,7 +7,7 @@ then
    value=$((value+1))
    echo $value > ./lastPort
    echo "Invoking docker run with $value"
-   docker run -e LISTEN_PORT=$value --net=host rtv2222/user-service-js
+   docker run -e LISTEN_PORT=$value --net=host rtv2222/user-service-js &
 else
    echo "Failed to build and deploy the docker container"
    exit 1
